e738a5529e0a20b8319a54472c8aa19f7bdd696d
Nova <ininopal222@gmail.com>
2026-02-27 22:46:27 +0700

    percobaan 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/hello', function () {
+    return ('hello');
+});
+
+Route::get('/world', function () {
+    return ('world');
+});
+Route::get('/user/{name}', function ($name) {
+    return 'nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/user/{name?}', function ($name='john') {
+return 'Nama saya '.$name;
 });
